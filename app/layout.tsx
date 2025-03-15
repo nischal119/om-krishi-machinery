318cbec00252de490d4da2b91d16151c904c97d3
nischal119 <dhungelnischal7@gmail.com>
2025-03-15 16:02:20 +0545

    added a favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,7 +91,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/Om logo png.png" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
 
